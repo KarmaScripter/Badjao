9ed1d2ce6a2d304cb781db8c2fc342c38fc0c44e
KarmaScripter <terry.eppler@gmail.com>
2022-10-19 07:16:25 -0400

    update from main rig;

---
+++ Controls/CalendarForm.cs
@@ -0,0 +1,16 @@
+﻿// // <copyright file = "CalendarForm.cs" company = "Terry D. Eppler">
+// // Copyright (c) Terry D. Eppler. All rights reserved.
+// // </copyright>
+
+using System.Windows.Forms;
+
+namespace BudgetExecution
+{
+    public partial class CalendarForm : Form
+    {
+        public CalendarForm( )
+        {
+            InitializeComponent( );
+        }
+    }
+}
