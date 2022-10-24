b25d2c96db744695697291006b0e90741a46106d
KarmaScripter <terry.eppler@gmail.com>
2022-10-24 04:46:28 -0400

    updates

--- Extensions/TypeObject.cs
+++ Extensions/TypeObject.cs
@@ -1,53 +1,22 @@
-﻿// // <copyright file = "TypeObject.cs" company = "Terry D. Eppler">
-// // Copyright (c) Terry D. Eppler. All rights reserved.
-// // </copyright>
+﻿// <copyright file = "TypeObject.cs" company = "Terry D. Eppler">
+// Copyright (c) Terry D. Eppler. All rights reserved.
+// </copyright>
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Json;
 
 namespace BudgetExecution
 {
-    using System;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Text;
-    using System.Xml.Serialization;
-    using System.IO;
-    using System.Runtime.Serialization;
-    using System.Runtime.Serialization.Formatters.Binary;
-    using System.Runtime.Serialization.Json;
-
     /// <summary>
     /// 
     /// </summary>
     [ SuppressMessage( "ReSharper", "CompareNonConstrainedGenericWithNull" ) ]
     public static class TypeObject
     {
-        /// <summary>
-        /// Copies the specified type.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="type">The type.</param>
-        /// <returns></returns>
-        public static T Copy<T>( this T type )
-            where T : ISerializable
-        {
-            if( type != null )
-            {
-                try
-                {
-                    using var _stream = new MemoryStream( );
-                    var _formatter = new BinaryFormatter( );
-                    _formatter.Serialize( _stream, type );
-                    _stream.Position = 0;
-                    return (T)_formatter.Deserialize( _stream );
-                }
-                catch( Exception ex )
-                {
-                    Fail( ex );
-                    return default( T );
-                }
-            }
-
-            return default( T );
-        }
-
         /// <summary>
         /// Converts to json.
         /// </summary>
@@ -85,16 +54,18 @@ namespace BudgetExecution
         /// <returns>
         /// A string.
         /// </returns>
-        public static string BinarySerialize<T>( this T type )
+        public static string JavaScriptSerialize<T>( this T type )
         {
             if( type != null )
             {
                 try
                 {
-                    var _formatter = new BinaryFormatter( );
+                    var _encoding = Encoding.Default;
+                    var _serializer = new DataContractJsonSerializer( typeof( T ) );
                     using var _stream = new MemoryStream( );
-                    _formatter.Serialize( _stream, type );
-                    return Encoding.Default.GetString( _stream.ToArray( ) );
+                    _serializer.WriteObject( _stream, type );
+                    var json = _encoding.GetString( _stream.ToArray( ) );
+                    return json;
                 }
                 catch( Exception ex )
                 {
